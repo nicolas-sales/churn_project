0be7572323a06b740972c65a96612d12081eff0c
nicolas-sales <nicolas.sales77@gmail.com>
2026-06-04 16:34:10 +0200

    cicd

---
+++ tests/test_api.py
@@ -0,0 +1,13 @@
+from fastapi.testclient import TestClient
+
+from src.api.app import app
+
+
+client = TestClient(app)
+
+
+def test_docs_available():
+
+    response = client.get("/docs")
+
+    assert response.status_code == 200
