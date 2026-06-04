3a98bb4297e44b11221241c74597411a4d56beb6
nicolas-sales <nicolas.sales77@gmail.com>
2026-06-04 16:55:25 +0200

    CICD

--- src/api/routes.py
+++ src/api/routes.py
@@ -8,14 +8,28 @@ from src.agent.agent import ChurnAgent
 
 router = APIRouter()
 
-pipeline = PredictionPipeline()
+# pipeline = PredictionPipeline()
 
-agent = ChurnAgent()
+# agent = ChurnAgent()
 
 # Endpoints
 
+@router.get("/")
+def root():
+    return {
+        "message" : "Customer Churn API is running"
+    }
+
+@router.get("/health")
+def health_check():
+    return {
+        "status" : "healthy"
+    }
+
 @router.post("/pipeline")
 def run_pipeline(customer: CustomerInput):
+
+    pipeline = PredictionPipeline()
 
     result = pipeline.run_pipeline(customer.model_dump()) # model.dump pour transformer un dictionnaire pydantic en dictionnaire python
 
@@ -23,6 +37,8 @@ def run_pipeline(customer: CustomerInput):
 
 @router.post("/analyse")
 def analyse_customer(customer: CustomerInput):
+
+    agent = ChurnAgent()
 
     result = agent.analyse_customer(customer.model_dump())
 
--- tests/test_api.py
+++ tests/test_api.py
@@ -5,6 +5,21 @@ from src.api.app import app
 
 client = TestClient(app)
 
+def test_root_endpoint():
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+    assert response.json()["message"] == "Customer Churn API is running"
+
+
+def test_health_endpoint():
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+    assert response.json()["status"] == "healthy"
+
 
 def test_docs_available():
 
